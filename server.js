ee0d6be3d9844bd166ad5cee3e7bc3545f3b3c29
veeresh <veereshgollapu@gmail.com>
2026-06-12 09:56:26 +0530

    changed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const userRoutes = require("./routes/users");
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://vereesh-style-hub.vercel.app/", credentials: true }));
 app.use(express.json());
 
 // Routes
